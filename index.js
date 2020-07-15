a4f5c3ee43c6bc2d4cc5001baf4742ddfd9be308
Ugyen Jigme Rangdrel <ugyen.jigme2018@gmail.com>
2020-07-15 19:38:49 +0600

    'Day-2'

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 //Variables:
 let backdrop = document.querySelector('#backdrop');
+let detail = document.getElementsByClassName('detail');
+let popup = document.querySelector('.popup');
 
 function init(){
     let widthValue = window.innerWidth;
@@ -12,4 +14,22 @@ init();
 window.addEventListener('resize', () => {
     init();
 })
+
+for(let i = 0; i < detail.length; i++){
+    detail[i].addEventListener('click', () => {
+        popup.addEventListener('click', ()=> {
+            popup.classList = 'popup';
+        })
+        
+        if(popup.classList.length === 2){
+            detail[i].removeEventListener('click', ()=>{});
+           
+        } else{
+            popup.classList.toggle('popup-active');
+        }
+        
+    })    
+}
+
+
 
